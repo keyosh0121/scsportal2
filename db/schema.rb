6e72927948168eeb5c32671826d33fc301b139fe
荒井風太 <arafubeatbox@gmail.com>
2019-01-07 17:52:20 +0900

    forgot to recreate database

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_06_124125) do
+ActiveRecord::Schema.define(version: 2019_01_07_085016) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,7 +31,7 @@ ActiveRecord::Schema.define(version: 2019_01_06_124125) do
     t.string "name", default: "", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
-    t.integer "year"
+    t.integer "year", default: 2019
     t.string "uni", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
