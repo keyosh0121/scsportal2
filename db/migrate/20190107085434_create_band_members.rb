8d89aeb485cf1e90dc09f6542627054cee79f987
荒井風太 <arafubeatbox@gmail.com>
2019-01-07 17:57:49 +0900

    create table bandmembers

---
+++ db/migrate/20190107085434_create_band_members.rb
@@ -0,0 +1,10 @@
+class CreateBandMembers < ActiveRecord::Migration[5.2]
+  def change
+    create_table :band_members do |t|
+      t.references :user, foreign_key: true, null: false
+      t.references :band, foreign_key: true, null: false
+      t.integer :mic_number, null: false
+      t.timestamps
+    end
+  end
+end
