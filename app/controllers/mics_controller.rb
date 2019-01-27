28674b82db5273a546607ed82645886a3cc8bf93
n.wada <jones3429@outlook.jp>
2019-01-28 00:20:35 +0900

    マイク練一覧(一般ユーザー用)

--- app/controllers/mics_controller.rb
+++ app/controllers/mics_controller.rb
@@ -1,2 +1,32 @@
+# どんさん担当
 class MicsController < ApplicationController
+  # マイク練一覧画面(マイク練係確認用)
+  def index
+  	@mics = Mic.where("date >= ?", Date.today).order(date: "ASC", period_id: "ASC")
+  end
+
+  # 申請フォームは後から他のやつを使い回すので大丈夫です。
+  def new
+  end
+
+  def create
+  end
+
+  # 自分のマイク練一覧画面
+  def show
+  	@mics = Mic.where("date >= ?", Date.today).where(band_id: User.find(params[:id]).bands.map(&:id)).order(date: "ASC", period_id: "ASC")
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  # 分割順番選択画面
+  def order
+  end
 end
