2fa61d981d84b79f534bdf21697f670a42b3f7ae
荒井風太 <arafubeatbox@gmail.com>
2019-01-27 11:39:36 +0900

    modify error messages

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,9 @@
 class User < ApplicationRecord
   belongs_to :section
+  validates :year, presence: true
+  validates :name, presence: true
+  validates :authority_id, presence: true
+  validates :uni, presence: true
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
