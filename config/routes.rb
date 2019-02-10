6c584980263414e469fe248402a5f58840a49389
吉山慶 <kei@yoshiyamakeinoMacBook-Pro.local>
2019-02-10 11:35:07 +0900

    room-usage/index added

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   resources :comments
   resources :presentations
   resources :events
-  resources :room_usages
+  resources :room_usages, only:[:index,:new]
   resources :mics
   resources :bands
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
