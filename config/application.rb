2718555c9bc0530b4c924a9b8c23c490deedd66f
荒井風太 <arafubeatbox@gmail.com>
2019-02-11 17:54:25 +0900

    delete compiled assets

--- config/application.rb
+++ config/application.rb
@@ -9,6 +9,7 @@ Bundler.require(*Rails.groups)
 module Scsportal2
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
+    config.serve_static_assets = true
     config.load_defaults 5.2
     config.time_zone = 'Tokyo'
     config.active_record.default_timezone = :local
