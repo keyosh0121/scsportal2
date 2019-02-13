2f0c11131f4814e9aeb7ae801c07a736ac157018
荒井風太 <arafubeatbox@gmail.com>
2019-02-13 16:25:41 +0900

    add active admin and bullet

---
+++ app/admin/authorities.rb
@@ -0,0 +1,18 @@
+ActiveAdmin.register Authority do
+
+  permit_params :name
+
+  index do
+    selectable_column
+    id_column
+    column :name
+  end
+
+  form do |f|
+    f.inputs do
+      f.input :name
+    end
+    f.actions
+  end
+
+end
