fb616018f413be5c15d4967fa244bc50000eec5a
n.wada <jones3429@outlook.jp>
2019-02-15 17:38:11 +0900

    マイク練分割用ルーティング追加

--- config/routes.rb
+++ config/routes.rb
@@ -10,4 +10,6 @@ Rails.application.routes.draw do
   resources :mics
   resources :bands
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/order/:id', to: 'mics#order'
+  post '/order/:id/:order', to: 'mics#order_update'
 end
