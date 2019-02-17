8b8fe2000625535f6cb6be275d96bf4973fdeb24
荒井風太 <arafubeatbox@gmail.com>
2019-02-17 15:58:51 +0900

    add user helper

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,31 @@
 module UsersHelper
+
+  def year_in_scs(user)
+    current_year = Date.today.year
+    current_month = Date.today.month
+    user_year = user.user_year
+
+    if 4 <= current_month
+      if current_year - user_year == 0
+        return "1年目"
+      elsif current_year - user_year == 1
+        return "2年目"
+      elsif current_year - user_year == 2
+        return "3年目"
+      elsif current_year - user_year == 3
+        return "4年目"
+      end
+    else
+      if current_year - user_year == 1
+        return "1年目"
+      elsif current_year - user_year == 2
+        return "2年目"
+      elsif current_year - user_year == 3
+        return "3年目"
+      elsif current_year - user_year == 4
+        return "4年目"
+      end
+    end
+  end
+
 end
--- app/views/users/search.json.jbuilder
+++ app/views/users/search.json.jbuilder
@@ -1,6 +1,6 @@
 json.array! @users do |user|
   json.name user.user_name
-  json.year user.user_year
+  json.year year_in_scs(user)
   json.section user.section.name
   json.id user.id
 end
